6f1f3f8ce854e9caf6b6f4807e3d99eb1b1fa024
Andres Cortés <158852844+ChatAI-123@users.noreply.github.com>
2025-02-10 16:54:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
 Program Name: main.js
 Name: Andres Cortes
 Date Created: 02/05/2025
-Date Last Edited: 02/09/2025
+Date Last Edited: 02/10/2025
 Version: 1.1
 Description: Homework 1 JS
 Credits: The skeleton of this JS code is from MISSO. No public link to cite.
 */
 
-//dynamic date js code
+// Dynamic Date
 const d = new Date();
 let text = d.toLocaleDateString();
 document.getElementById("today").innerHTML = text;
 
-//name slider js code
+// Name Slider Value
 let slider = document.getElementById("visitSatisfactionRating");
 let output = document.getElementById("rating-slider");
 output.innerHTML = slider.value;
